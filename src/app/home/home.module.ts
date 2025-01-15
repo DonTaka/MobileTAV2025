88caca849ad609d3484a7a92fa8f3821a31a061f
j.riquelmee <101286474+DonTaka@users.noreply.github.com>
2025-01-15 16:49:05 -0300

    Semana 3
    Instalacion Angular Material
    Implementacion material Spinner
    Animaciones con Ionic
    Ejemplo de animacion con Ionic Animations

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,15 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    MatProgressSpinner,
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
+import { AnimationController } from '@ionic/angular';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -13,16 +15,87 @@ export class HomePage {
   };
 
   msj = '';
-  constructor(private router: Router) {}
+  carga = false;
+  constructor(private router: Router, private animation: AnimationController) {}
 
   conectar() {
     if (this.user.usuario.length > 0 && this.user.password.length > 0) {
-      let navigationExtras: NavigationExtras = {
-        state: { user: this.user },
-      };
-      this.router.navigate(['/perfil'], navigationExtras);
+      if (
+        this.user.usuario == 'j.riquelmee' &&
+        this.user.password == 'pass1234'
+      ) {
+        let navigationExtras: NavigationExtras = {
+          state: { user: this.user },
+        };
+        this.carga = true;
+        this.msj = 'Conexion Exitosa';
+        /* setTimeout permite generar un delay en MS */
+        setTimeout(() => {
+          this.router.navigate(['/perfil'], navigationExtras);
+          this.msj = '';
+          this.carga = false;
+        }, 3000);
+      } else {
+        this.msj = 'Credenciales erroneas';
+      }
     } else {
       this.msj = 'Credenciales no pueden estar vacias';
     }
+  }
+
+  ngAfterContentInit() {
+    this.animacionLogin();
+  }
+
+  animacionLogin() {
+    /* Seleccionamos el elemento que deseamos utilizar para la animacion
+       POr medio de un querySelector
+    */
+    const imagen = document.querySelector(
+      '#container ion-card ion-card-header ion-img'
+    ) as HTMLElement;
+    /* Una vez seleccionamos , generamos la animacion por medio del animation controller
+      Rectificar cada atributo en la documentacion 
+      https://ionicframework.com/docs/utilities/animations
+    */
+    const animacion = this.animation
+      .create()
+      .addElement(imagen)
+      .duration(5000)
+      .iterations(Infinity)
+      .keyframes([
+        {
+          offset: 0,
+          opacity: '1',
+          border: '10px solid white',
+          transform: 'translateX(0px)',
+        },
+        {
+          offset: 0.25,
+          opacity: '0.5',
+          border: '10px solid red',
+          transform: 'translateX(100px)',
+        },
+        {
+          offset: 0.5,
+          opacity: '1',
+          border: '10px solid blue',
+          transform: 'translateX(0px)',
+        },
+        {
+          offset: 0.75,
+          opacity: '1',
+          border: '10px solid green',
+          transform: 'translateX(-100px)',
+        },
+        {
+          offset: 1,
+          opacity: '1',
+          border: '10px solid cyan',
+          transform: 'translateX(0px)',
+        },
+      ]);
+    /* Por ultimo le damos play a la animacion para que empiece */
+    animacion.play();
   }
 }
